0a7b5c950fec76d4ce7fdbb2d7806f45cc321937
Luciana <laf.0182@gmail.com>
2024-06-18 04:32:05 -0300

    Se agrega combo para actores/protagonistas y se modifican etiquetas de thymeleaf deprecadas

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -5,3 +5,5 @@ INSERT INTO GENEROS(NOMBRE) VALUES('Ciencia Ficcion');
 INSERT INTO GENEROS(NOMBRE) VALUES('Terror');
 INSERT INTO ACTORES(NOMBRE, URL_IMAGEN) VALUES('Keanu Reeves', 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Keanu_Reeves_2019.jpg/800px-Keanu_Reeves_2019.jpg');
 INSERT INTO ACTORES(NOMBRE, URL_IMAGEN) VALUES('Ashton Kutcher', 'https://phantom-marca.unidadeditorial.es/75902381b19fbed087bc681a0316f037/resize/1200/f/webp/assets/multimedia/imagenes/2022/11/08/16679317563720.jpg');
+INSERT INTO ACTORES(NOMBRE, URL_IMAGEN) VALUES('Michael J. Fox', 'https://cdn.britannica.com/33/130633-050-DA6DF1CF/Michael-J-Fox-activist.jpg');
+INSERT INTO ACTORES(NOMBRE, URL_IMAGEN) VALUES('Christopher Lloyd', 'https://ayeryhoyrevista.com/wp-content/uploads/2022/11/mundo-curioso-que-fue-de.jpg');
---
+++ src/main/resources/static/js/scripts.js
@@ -0,0 +1,59 @@
+//Obtengo valores de lo seleccionado en el select de actores
+function actorSelected(select){
+	let index = select.selectedIndex;
+	let option = select.options[index];
+	let id = option.value;
+	let nombre = option.text;
+	let urlImagen = option.dataset.url;
+
+//Deshabilito el item option xq cuando tengamos un actor, lo va a agregar y ya no lo vamos a poder volver
+//a agregar xq ya va a estar asociado con la pelicula
+	option.disabled = "disabled";
+	
+//Hago que el combo vuelva a la posición 0 p/que nos permita seleccionar más actores
+	select.selectedIndex = 0;
+	
+agregarActor(id, nombre, urlImagen)
+	
+//Obtenemos los ids de los actores que tengamos en el input Ids
+	let ids = $("#ids").val();
+	if(ids == ""){ //si el id tiene comillas, es xq no tiene ninguno. Entonces va a ser el primero/lo creamos.
+		$("#ids").val(id);
+	}else{ //en caso que ya tenga algún id, lo concatenamos
+		$("#ids").val(ids + "," + id);
+	} 
+	
+}
+
+function agregarActor(id, nombre, urlImagen){
+	let htmlString = `
+		<div class= "card col-md-3 m-2" style= "width: 10rem">
+			<img src="{URL-IMAGEN}" class= "card-img-top">
+			<div class= "card-body">
+				<p class="card-text">{NOMBRE}</p>
+				<button class="btn btn-danger" data-id="{ID}" onclick="eliminarActor(this); return false;"> Eliminar </button>
+			</div>
+		</div>`;
+		
+		htmlString = htmlString.replace("{URL-IMAGEN}", urlImagen );
+		htmlString = htmlString.replace("{NOMBRE}", nombre);
+		htmlString = htmlString.replace("{ID}", id);
+		
+		//Una vez reemplazados los valores, lo metemos en nuestro contenedor de protagonistas
+		$("#protagonistas_container").append(htmlString);
+}
+
+function eliminarActor(btn){
+	let id = btn.dataset.id;
+	let node = btn.parentElement.parentElement;
+	let arrayIds = $("#ids").val().split(",").filter(idActor => idActor != id);
+	//1,2,3,10 => [1,2,3,10] => supongamos que es el 3 => [1,2,10]
+	
+	$("#ids").val(arrayIds.join(",")); //hacemos la operación inversa de split, con join.
+	//"1,2,10"
+	
+	//Ahora volvemos a hablitar la opción del combo de actores
+	$("#protagonistas option[value='" + id + "']").prop("disabled", false);
+	
+	$(node).remove();
+}
